8a39a0dfcdca491b3d6972776854f1a212f8858f
lonzolyn <99560335+lonzolyn@users.noreply.github.com>
2023-03-23 16:59:04 -0400

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url,leftPx,bottomPx){
+    let object =  document.createElement('img')
+    object.src = url;
+    object.style.position = 'fixed'
+    object.style.left = leftPx + 'Px'
+    object.style.bottom = bottomPx + 'Px'
+    document.body.append(object)
+    return object
+}
+
+function newItem(url,leftPx,Px) {
+    let object = newImage(url, leftPx, bottomPx)
+
+    object.addEventListener('dblclick',() =>{
+        object.remove()
+    })
+}
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png',200, 300)
+newImage('assets/pillar.png', 350,100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assests/well.png', 500, 425)
+
+newItem('assets/sword.png',500,405)
+newItem('assets/sheild.png', 165, 185)
+newItem('asset/staff.png', 600, 100)
+
+
+
+    
