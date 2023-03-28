37138394ac40f76349150a26b73bbca40abc0237
lonzolyn <99560335+lonzolyn@users.noreply.github.com>
2023-03-28 14:20:06 -0400

    new changes

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ function newImage(url,leftPx,bottomPx){
     return object
 }
 
-function newItem(url,leftPx,Px) {
-    let object = newImage(url, leftPx, bottomPx)
+function newItem(url,left,bottom) {
+    let object = newImage(url, left, bottom)
 
     object.addEventListener('dblclick',() =>{
         object.remove()
     })
 }
 newImage('assets/green-character.gif', 100, 100)
-newImage('assets/tree.png',200, 300)
+newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350,100)
 newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/crate.png', 150, 200)
